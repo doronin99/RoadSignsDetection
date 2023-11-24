1d862148732863c4b875f562f5d9b4f00d3caf1c
Artem Kovalev <artem@kovalev.cc>
2023-11-24 16:59:00 +0300

    minor changes

--- utilities.py
+++ utilities.py
@@ -13,19 +13,19 @@ def create_yaml_from_class_map(class_map_path, yaml_output_path):
     :param class_map_path: path to the class_map.json file.
     :param yaml_output_path: path to the output YAML file.
     """
-    with open(class_map_path, 'r') as file:
+    with open(class_map_path, "r") as file:
         class_map = json.load(file)
 
     adjusted_class_map = {int(value - 1): key for key, value in class_map.items()}
 
     yaml_data = {
-        'path': './',
-        'train': 'train',
-        'val': 'valid',
-        'names': adjusted_class_map
+        "path": "./",
+        "train": "train",
+        "val": "valid",
+        "names": adjusted_class_map,
     }
 
-    with open(yaml_output_path, 'w') as yaml_file:
+    with open(yaml_output_path, "w") as yaml_file:
         yaml.safe_dump(yaml_data, yaml_file, sort_keys=False)
 
 
@@ -46,18 +46,20 @@ def clean_data(annotation_folder, min_limit, max_limit):
 
     files_to_delete = set()
     for file, classes in file_to_classes.items():
-        delete_file = any(class_counts[class_id] < min_limit for class_id in classes) or \
-                      any(class_counts[class_id] > max_limit for class_id in classes)
+        delete_file = any(
+            class_counts[class_id] < min_limit for class_id in classes
+        ) or any(class_counts[class_id] > max_limit for class_id in classes)
         if delete_file:
             files_to_delete.add(file)
 
     # Удаление файлов
     for file in files_to_delete:
         if os.path.exists(file):
-            os.remove(file)  
+            os.remove(file)
             jpg_file = file.replace(".txt", ".jpg")
             if os.path.exists(jpg_file):
-                os.remove(jpg_file) 
+                os.remove(jpg_file)
+
 
 def reduce_background_images(annotation_folder, image_folder, keep_percentage=0.1):
     """
@@ -70,10 +72,10 @@ def reduce_background_images(annotation_folder, image_folder, keep_percentage=0.
     total_files = 0
 
     for filename in os.listdir(annotation_folder):
-        if filename.endswith('.txt'):
+        if filename.endswith(".txt"):
             total_files += 1
             file_path = os.path.join(annotation_folder, filename)
-            with open(file_path, 'r') as file:
+            with open(file_path, "r") as file:
                 if not file.read().strip():  # Файл пуст
                     background_files.append(filename)
 
@@ -83,10 +85,9 @@ def reduce_background_images(annotation_folder, image_folder, keep_percentage=0.
     if num_to_remove > 0:
         files_to_remove = random.sample(background_files, num_to_remove)
         for filename in files_to_remove:
-            os.remove(os.path.join(annotation_folder, filename))  
-            image_filename = filename.replace('.txt', '.jpg')  
-            os.remove(os.path.join(image_folder, image_filename))  
-
+            os.remove(os.path.join(annotation_folder, filename))
+            image_filename = filename.replace(".txt", ".jpg")
+            os.remove(os.path.join(image_folder, image_filename))
 
 
 def remove_small_classes(annotation_folder, min_objects):
@@ -108,15 +109,12 @@ def remove_small_classes(annotation_folder, min_objects):
     for file, classes in file_to_classes.items():
         if any(class_counts[class_id] < min_objects for class_id in classes):
             os.remove(file)
-            image_file = file.replace('.txt', '.jpg')
+            image_file = file.replace(".txt", ".jpg")
             if os.path.exists(image_file):
                 os.remove(image_file)
 
 
-
-
-
-def split_data(source_folder, train_folder, valid_folder,seed=42, train_size=0.8):
+def split_data(source_folder, train_folder, valid_folder, seed=42, train_size=0.8):
     """
     Разбивает данные на тренировочную и валидационную выборки.
     :param source_folder: Source folder with images and annotations
@@ -126,7 +124,7 @@ def split_data(source_folder, train_folder, valid_folder,seed=42, train_size=0.8
     :param seed: random seed
     """
     random.seed(seed)
-    files = [file for file in os.listdir(source_folder) if file.endswith('.jpg')]
+    files = [file for file in os.listdir(source_folder) if file.endswith(".jpg")]
     random.shuffle(files)
 
     train_count = int(len(files) * train_size)
@@ -138,21 +136,28 @@ def split_data(source_folder, train_folder, valid_folder,seed=42, train_size=0.8
 
     for file in train_files:
         shutil.copy(os.path.join(source_folder, file), os.path.join(train_folder, file))
-        shutil.copy(os.path.join(source_folder, file.replace('.jpg', '.txt')), os.path.join(train_folder, file.replace('.jpg', '.txt')))
+        shutil.copy(
+            os.path.join(source_folder, file.replace(".jpg", ".txt")),
+            os.path.join(train_folder, file.replace(".jpg", ".txt")),
+        )
 
     for file in valid_files:
         shutil.copy(os.path.join(source_folder, file), os.path.join(valid_folder, file))
-        shutil.copy(os.path.join(source_folder, file.replace('.jpg', '.txt')), os.path.join(valid_folder, file.replace('.jpg', '.txt')))
+        shutil.copy(
+            os.path.join(source_folder, file.replace(".jpg", ".txt")),
+            os.path.join(valid_folder, file.replace(".jpg", ".txt")),
+        )
+
 
 def remove_classes_from_yaml(yaml_path, top_classes):
     """
     Deletes classes from a YAML file.
     """
-    with open(yaml_path, 'r') as file:
+    with open(yaml_path, "r") as file:
         data = yaml.safe_load(file)
 
-    if 'names' in data and isinstance(data['names'], dict):
-        index_to_name = data['names']
+    if "names" in data and isinstance(data["names"], dict):
+        index_to_name = data["names"]
 
         updated_classes = {}
         for new_index, old_index in enumerate(top_classes):
@@ -162,56 +167,64 @@ def remove_classes_from_yaml(yaml_path, top_classes):
                 print(f"Class index {old_index} not found in YAML 'names'")
 
         sorted_classes = {str(k): v for k, v in sorted(updated_classes.items())}
-        data['names'] = sorted_classes
+        data["names"] = sorted_classes
     else:
-        raise ValueError("YAML file format is not as expected. 'names' should be a dictionary.")
+        raise ValueError(
+            "YAML file format is not as expected. 'names' should be a dictionary."
+        )
 
-    with open(yaml_path, 'w') as file:
+    with open(yaml_path, "w") as file:
         yaml.safe_dump(data, file, sort_keys=False)
 
+
 import os
+
 
 def filter_annotations(annotation_folder, top_classes, threshold):
     class_mapping = {old: new for new, old in enumerate(top_classes)}
     class_counts = {class_id: 0 for class_id in top_classes}
 
     for filename in os.listdir(annotation_folder):
-        if filename.endswith('.txt'):
+        if filename.endswith(".txt"):
             file_path = os.path.join(annotation_folder, filename)
-            with open(file_path, 'r') as file:
+            with open(file_path, "r") as file:
                 lines = file.readlines()
 
             updated_lines = []
             for line in lines:
                 parts = line.split()
                 class_index = int(parts[0])
-                if class_index in class_mapping and class_counts[class_index] < threshold:
+                if (
+                    class_index in class_mapping
+                    and class_counts[class_index] < threshold
+                ):
                     class_counts[class_index] += 1
                     parts[0] = str(class_mapping[class_index])
-                    updated_lines.append(' '.join(parts) + '\n')
+                    updated_lines.append(" ".join(parts) + "\n")
 
             if updated_lines:
-                with open(file_path, 'w') as file:
+                with open(file_path, "w") as file:
                     file.writelines(updated_lines)
             else:
                 os.remove(file_path)
-                jpg_file_path = os.path.splitext(file_path)[0] + '.jpg'
+                jpg_file_path = os.path.splitext(file_path)[0] + ".jpg"
                 if os.path.exists(jpg_file_path):
                     os.remove(jpg_file_path)
 
     return class_mapping
 
+
 def update_yaml_classes(yaml_path, class_mapping):
-    with open(yaml_path, 'r') as file:
+    with open(yaml_path, "r") as file:
         yaml_data = yaml.safe_load(file)
 
-    if 'names' in yaml_data:
+    if "names" in yaml_data:
         updated_classes = {}
         for old_index, new_index in class_mapping.items():
-            if old_index in yaml_data['names']:
-                updated_classes[new_index] = yaml_data['names'][old_index]
+            if old_index in yaml_data["names"]:
+                updated_classes[new_index] = yaml_data["names"][old_index]
 
-        yaml_data['names'] = updated_classes
+        yaml_data["names"] = updated_classes
 
-    with open(yaml_path, 'w') as file:
+    with open(yaml_path, "w") as file:
         yaml.safe_dump(yaml_data, file, sort_keys=False)
